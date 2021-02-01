c172dbfd28ae86f375c9fe4d5836c2a8f2a0c64c
fantasticfbaby <1092902073@qq.com>
2021-02-02 00:11:30 +0800

    update

--- test/test.js
+++ test/test.js
@@ -1,5 +1,47 @@
-function func(a,b,c,){
-  console.log(arguments);
+function arrayInclude (arrA, arrB) {
+  function getMap (arr) {
+    let m = new Map()
+    for (let key of arr) {
+      if (m.has(key)) {
+        m.set(key, m.get(key)+1)
+      } else {
+        m.set(key, 1)
+      }
+    }
+    return m
+  }
+  let mapA = getMap(arrA)
+  let interSection = []
+  for (let v of arrB) {
+    let count = mapA.get(v) || 0
+    if (count > 0) {
+      interSection.push(v)
+      mapA.set(v, count-1)
+    }
+  }
+
+  let lenIS = interSection.length, lenA = arrA.length, lenB = arrB.length
+  let aAllInB = lenA === lenIS
+  let bAllInA = lenB === lenIS
+  if (aAllInB && bAllInA) return 0
+  if (aAllInB) return 1
+  if (bAllInA) return 2
+  return -1
 }
 
-func(1,2,3)
+var a1 = [4,2,3,1,4]
+var a2 = [4,2,3,1,4,5]
+console.log(arrayInclude(a1, a2)) // 1
+
+var a3 = [4,2,3,1,4]
+var a4 = [4,2,3,1]
+console.log(arrayInclude(a3, a4)) // 2
+
+var a5 = [4,2,3,1,4]
+var a6 = [4,2,3,1,4]
+console.log(arrayInclude(a5, a6)) // 0
+
+var a7 = [4,2,3,1,4]
+var a8 = [3,2,3,1,4]
+console.log(arrayInclude(a7, a8)) // -1
+
--- test/test2.js
+++ test/test2.js
@@ -1,18 +1,30 @@
-import React from "react";
-
-const EMPTY = Symbol();
-
-export function createContainer(useHook) {
-  const Context = React.createContext(EMPTY);
-  function Provider(props) {
-    let value = useHook(props.initialState);
-    return <Context.Provider value={value}></Context.Provider>;
-  }
-
-  function useContainer() {
-    let value = React.useContext(Context);
-    return value;
-  }
-
-  return { Provider, useContainer };
-}
+new Promise(function f1(resolve, reject) {
+  console.log(1);
+  resolve();
+})
+  .then(function f2() {
+    console.log(2);
+    new Promise(function f3(resolve, reject) {
+      console.log(3);
+      resolve();
+    })
+      .then(function f4() {
+        console.log(4);
+        new Promise(function f5(resolve, reject) {
+          console.log(5);
+          resolve();
+        })
+          .then(function f7() {
+            console.log(7);
+          })
+          .then(function f9() {
+            console.log(9);
+          });
+      })
+      .then(function f8() {
+        console.log(8);
+      });
+  })
+  .then(function f6() {
+    console.log(6);
+  });
