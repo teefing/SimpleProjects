8188ec06f0eefc30bfc6ebb5fdd5a28edf2094be
fantasticfbaby <1092902073@qq.com>
2021-02-28 16:24:29 +0800

    update

--- test/test2.js
+++ test/test2.js
@@ -1,30 +1,5 @@
-new Promise(function f1(resolve, reject) {
-  console.log(1);
-  resolve();
+let promise = new Promise((resolve, reject) => {
+  resolve(1)
+}).then(2).then(val => {
+  console.log(val);
 })
-  .then(function f2() {
-    console.log(2);
-    new Promise(function f3(resolve, reject) {
-      console.log(3);
-      resolve();
-    })
-      .then(function f4() {
-        console.log(4);
-        new Promise(function f5(resolve, reject) {
-          console.log(5);
-          resolve();
-        })
-          .then(function f7() {
-            console.log(7);
-          })
-          .then(function f9() {
-            console.log(9);
-          });
-      })
-      .then(function f8() {
-        console.log(8);
-      });
-  })
-  .then(function f6() {
-    console.log(6);
-  });
